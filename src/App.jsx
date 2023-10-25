fe35192b18242098998d8be7929690f9281561bb
Tanaya7989 <111045719+Tanaya7989@users.noreply.github.com>
2023-10-25 19:10:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Data from './Data'
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+  const[data,setData]=useState(Data)
+  return (
+    <div className="App">
+       <section>
+        <main>
+        <p>Today's Birthday {Data.length}🎂💐</p>
+          {data.map((x)=>{
+            return(
+              <div className="Cards" key={x.age}>
+                <img src={x.image} alt='' height={'70px'} width={'70px'}></img>
+                <ruby><p>{x.age}</p><rt>{x.name}</rt></ruby>
+              </div>
+            )
+          })}
+          <button onClick={()=>setData([])}>Clear</button>
+        </main>
+      </section>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,46 @@
+const Data = [
+   {id:1,
+    name:"Rohit Sharma",
+    age:"36",
+    image:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi2gcyXIxIBobfkbrFjYoHF1xhFUTyMO6btQ&usqp=CAU"
+   },
+   {id:2,
+    name:"Salman Khan",
+    age:"57",
+    image:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE4HpRv_RhFlLP3OnRkaeRDlo_ZiolVtFHZg&usqp=CAU"
+   },
+   {id:3,
+    name:"Yuvraj Sing",
+    age:"42",
+    image:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Yuvraj_Singh_appointed_as_Ulysse_Nardin_watch_brand_ambassador.jpeg/330px-Yuvraj_Singh_appointed_as_Ulysse_Nardin_watch_brand_ambassador.jpeg"
+   },
+   {id:4,
+    name:"P V Sindhu",
+    age:"28",
+    image:
+    "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRecvtW0n8hcEhF9QKGtm_eX_wfBADdbQ62f-Zc5VD8CZlPABqEp9SYjN9U2pbKzdJQ"
+   },
+   {id:5,
+    name:"Prabhas",
+    age:"44",
+    image:
+    'https://www.bornglorious.com/newthumbs/31/36/3136554.jpg'
+   },
+   {id:6,
+    name:"Kriti Sanon",
+    age:"33",
+    image:
+    'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/go8GUz7poytTvm9P9HNHQ3T84N1.jpg'
+   },
+   {id:7,
+    name:"Akshay Kumar",
+    age:"57",
+    image:
+    'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRX2X3ETcdbTbkrEqi6hMHDJuQgOYovj8TnGG5Jn5iNEpDe-GGN'
+   }
+
+]
+export default Data
